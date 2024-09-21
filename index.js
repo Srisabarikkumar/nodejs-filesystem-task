aceab4b14c27532b4833eebff1bcfdf717316388
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2024-09-21 10:00:49 +0530

    files added

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const express = require("express");
+const { format } = require("date-fns");
+const path = require("path");
+const app = express();
+app.get("/", (req, res) => {
+  const currTime = format(new Date(), "dd-MM-yyyy-HH-mm-ss");
+  const addCurrTime = path.join("Current-timestamp", `${currTime}.txt`);
+  fs.writeFileSync(addCurrTime, currTime, "utf-8");
+  fs.readFileSync(addCurrTime, "utf-8");
+  res.status(200).send(`Current Timestamp: ${currTime}.
+    Go to /getFiles endpoint to get all the files`);
+});
+app.get("/getFiles", (req, res) => {
+  fs.readdir("Current-timestamp", (err, files) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error occured");
+    } else {
+      const getFiles = files.filter((file) => path.extname(file) === ".txt");
+      res.status(200).send(getFiles);
+    }
+  });
+});
+app.listen(3000, () => {
+  console.log("App is running on port 3000");
+});
